c3ad7d73e7dcd850229497b4734b4ccb76aade36
xiaodu007 <weihao@0917.com>
2025-03-21 17:26:56 +0800

    first commit

---
+++ src/main/java/com/research/manager/ldm/LdmApplication.java
@@ -0,0 +1,13 @@
+package com.research.manager.ldm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/research/manager/ldm/LdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.research.manager.ldm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
