ad1f6162f543a0d0111d7248069ebf0273c24a0e
xiaodu007 <weihao@0917.com>
2025-04-09 18:13:44 +0800

    多模块改造
    Signed-off-by: xiaodu007 <weihao@0917.com>

---
+++ ldm-manager/src/test/java/com/research/manager/sysmanager/SysmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.research.manager.sysmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
