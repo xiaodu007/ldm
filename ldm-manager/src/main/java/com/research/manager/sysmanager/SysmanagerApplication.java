61abf78ed315ac74c7022f5f48eb9dd6eb19974b
xiaodu007 <weihao@0917.com>
2025-04-21 11:12:23 +0800

    整合nacos
    Signed-off-by: xiaodu007 <weihao@0917.com>

--- ldm-manager/src/main/java/com/research/manager/sysmanager/SysmanagerApplication.java
+++ ldm-manager/src/main/java/com/research/manager/sysmanager/SysmanagerApplication.java
@@ -3,9 +3,11 @@ package com.research.manager.sysmanager;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = {"com.research.manager.*"})
 @MapperScan("com.research.manager.sysmanager.mapper")
+@EnableDiscoveryClient
 public class SysmanagerApplication {
 
     public static void main(String[] args) {
